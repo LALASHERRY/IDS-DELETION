26aebedd1e4c92e83f7664cfc7b92c0e4e10e18a
SK-AK47 <104360717+LALASHERRY@users.noreply.github.com>
2023-08-30 18:12:44 +0500

    DELETE.py

--- DELETE.py
+++ DELETE.py
@@ -1,16 +1,1 @@
-import os, sys, platform,time
-try:
-    import requests
-except:
-    os.system('pip install requests')
- 
-bit = platform.architecture()[0]
-if bit == '64bit':
-    os.system('clear')
-    os.system('git pull')
-    import DELETE64
-elif bit == '32bit':
-    os.system('clear')
-    os.system('git pull')
-    import DELETE32
- 
+import DELETE
