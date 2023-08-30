822c179a89c3222eb5012d9efe3c8d354250f0f3
SK-AK47 <104360717+LALASHERRY@users.noreply.github.com>
2023-08-30 18:01:03 +0500

    Create DELETE.py

---
+++ DELETE.py
@@ -0,0 +1,12 @@
+import requests
+ except:
+ 
+     os.system('pip install requests')
+os.system('xdg-open https://facebook.com/groups/209798984167790/')
+ 
+ 
+ bit = platform.architecture()[0]
+
+ 
+ elif bit == '32bit':
+    import DELETE32
